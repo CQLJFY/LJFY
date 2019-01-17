f3f79ab496c048313eef57bcf676f4e8528f6569
小胖子 <917146738@qq.com>
2019-01-17 15:33:15 +0800

    2019-1-17

--- apps/data/views.py
+++ apps/data/views.py
@@ -10,12 +10,14 @@ class pj_data(View):
         search=request.GET.get("search","")
         if search:
             all_pj=Surveyattribute.objects.filter(pjid__icontains=search)
-            print(search)
+            title_pj = Surveyattribute.objects.filter(pjid__icontains=search)[:1]
         else:
             all_pj=None
+            title_pj=None
 
         return render(request, 'data_search.html', {
-            "all_pj":all_pj
+            "all_pj":all_pj,
+            "title_pj":title_pj,
 
         })
 
