f0c3990e95fbf274ce1f7f232592ed97b36ec07e
小胖子 <917146738@qq.com>
2019-01-18 11:56:08 +0800

    2019-1-18  1

--- apps/data/views.py
+++ apps/data/views.py
@@ -9,14 +9,23 @@ class pj_data(View):
     def get(self, request):
         search=request.GET.get("search","")
         if search:
+            # 取出搜索的所有工程
             all_pj=Surveyattribute.objects.filter(pjid__icontains=search)
+            # 取出搜索工程的id
+            said_ids=[pj.idsurveyattribute for pj in all_pj]
+            # 取出搜索工程的所有文件
+            all_file=Surveyfile.objects.filter(said__pjid__icontains=search)
+
             title_pj = Surveyattribute.objects.filter(pjid__icontains=search)[:1]
         else:
             all_pj=None
             title_pj=None
+            all_file=None
 
-        return render(request, 'data_search.html', {
+
+        return render(request, 'test.html', {
             "all_pj":all_pj,
             "title_pj":title_pj,
+            "all_file":all_file
         })
 
