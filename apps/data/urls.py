b083b06b7f3b617e209d04e8e6a06c5be871112b
小胖子 <917146738@qq.com>
2019-01-27 10:31:52 +0800

    2019-1-26 5

--- apps/data/urls.py
+++ apps/data/urls.py
@@ -2,13 +2,13 @@ __author__ = 'zhu'
 __date__ = '2018/10/24 14:43'
 
 from django.urls import path,re_path
-from .views import pj_data,analysis_report_down
+from .views import pj_data,files_download
 
 urlpatterns = [
     # 项目数据
     path('data/', pj_data.as_view(), name='pj_data'),
-    # 下载分析报告
-    re_path('download/(?P<idsurveyattribute>\d+)',analysis_report_down,name='download'),
+    # 下载文件
+    re_path('download/(?P<idsurveyattribute>\d+)/(?P<type>\d+)/',files_download,name='download'),
 
 
 ]
--- apps/data/views.py
+++ apps/data/views.py
@@ -1,4 +1,5 @@
 import json
+import zipfile
 from django.shortcuts import render,render_to_response
 from django.db.models import Q
 import os
@@ -65,32 +66,6 @@ class pj_data(View):
             # 去重
             all_person = set(all_person)
 
-            # 将所有需要下载的资料分别写出文件
-            # 创建目录
-            new_file_path=''
-            for i in title_pj:
-                new_file_path = "F:\\LJFY\\files_download\\" + i.pjid + '\\' + i.surveyperson + '\\' \
-                                + i.filetype + '\\'
-                if not os.path.exists(new_file_path):  #判断文件是否存在
-                    os.makedirs(new_file_path)  #创建文件
-            # 取出外业文件
-            if UserProfile.objects.filter(username=request.user)[0].user_type == "小组长":
-                all_files = Surveyfile.objects.filter(Q(said__pjid__icontains=search),Q(said__surveyperson=request.user))
-            else:
-                all_files = Surveyfile.objects.filter(said__pjid__icontains=search)
-            # 将外业文件写出来
-            for i in all_files:
-                file_path = os.path.split(i.filepath)
-                with open(new_file_path + file_path[1],'wb') as f:
-                    f.write(bytes(i.content))
-
-            # 取出点位数据
-            file_points=Points2018.objects.filter(source__pjid__icontains=search)
-            with open(new_file_path + '点位数据.txt','w') as f:
-                for i in file_points:
-                    f.write(i.wkbgeometry+','+i.source.pjid)
-                    f.write('\n')
-
         else:
             title_pj=None
             list=[]
@@ -103,46 +78,86 @@ class pj_data(View):
         })
 
 
-def analysis_report_down(request,idsurveyattribute):
-    '''将分析结果取出来，写成文件并下载'''
-    # 取出工程文件
-    obj=Surveyfile.objects.filter(Q(said__idsurveyattribute=idsurveyattribute),
-                                                   Q(filepath__contains='.cpf') |
-                                                   Q(filepath__contains='.svy') |
-                                                   Q(filepath__contains='.dc'))
-    new_file_path = ''
+def files_download(request,idsurveyattribute,type):
+    '''判断type，把对应的数据写成文件并下载，1是分析报告，2是点位数据，3是外业文件'''
+    def zip_ya(startdir,file_news):
+        '''文件压缩'''
+        z = zipfile.ZipFile(file_news, 'w', zipfile.ZIP_DEFLATED)  # 参数一：文件夹名
+        for dirpath, dirnames, filenames in os.walk(startdir):
+            fpath = dirpath.replace(startdir, '')  # 这一句很重要，不replace的话，就从根目录开始复制
+            fpath = fpath and fpath + os.sep or ''  # 这句话理解我也点郁闷，实现当前文件夹以及包含的所有文件的压缩
+            for filename in filenames:
+                z.write(os.path.join(dirpath, filename), fpath + filename)
+                print('压缩成功')
+        z.close()
+    new_file_path = '' #每个文件最终的绝对路径
+    file_name=''
+    file_path=''# 工程文件夹的绝对路径
+    obj = Surveyfile.objects.filter(Q(said__idsurveyattribute=idsurveyattribute),
+                                    Q(filepath__contains='.cpf') |
+                                    Q(filepath__contains='.svy') |
+                                    Q(filepath__contains='.dc'))
     for i in obj:
-        # 将文件路径简化
-        i.filepath = i.filepath.split('\\')[7:]
-        str = ' / '
-        i.filepath = str.join(i.filepath)
-        # 完成状态映射
-        if i.said.isanalysed == 1:
-            i.said.isanalysed = '已分析'
-        elif i.said.isanalysed == 0:
-            i.said.isanalysed = '未分析'
-        else:
-            i.said.isanalysed = '文件不全'
-        # 下载文件夹的绝对路径
-        new_file_path = "F:\\LJFY\\files_download\\" + i.said.pjid + '\\' + i.said.surveyperson + '\\' \
+        file_path = "F:\\LJFY\\files_download\\" + i.said.pjid + '\\' + i.said.surveyperson + '\\' \
                         + i.said.filetype + '\\'
-        if not os.path.exists(new_file_path):  # 判断文件夹是否存在
-            os.makedirs(new_file_path)  # 创建文件
-    # 将检查数据写成文件
-    all_check = CheckInformation.objects.filter(source__idsurveyattribute=idsurveyattribute). \
-        order_by("-log_level")
-    with open(new_file_path + '分析报告.txt', 'w') as f:
-        for i in all_check:
-            f.write(i.source.pjid + ' ' + i.source.pjname + ' 数据类型：' + i.source.filetype + ' 状态：'+ '\n')
-            # f.write(i.source.filepath + '\n')
-            for j in all_check:
-                f.write(j.log_level + ' : ' + j.check_category + '  ' + j.detail + '  ' + '\n')
-            break
-    file_name = "分析报告.txt"  # 文件名
-    new_file_path = os.path.join(new_file_path,file_name)  # 下载文件的绝对路径
+        if not os.path.exists(file_path):  # 判断文件夹是否存在
+            os.makedirs(file_path)  # 创建文件
+    if type=='1':
+        # 取出工程文件
+        for i in obj:
+            # 将文件路径简化
+            i.filepath = i.filepath.split('\\')[7:]
+            str = ' / '
+            i.filepath = str.join(i.filepath)
+            # 完成状态映射
+            if i.said.isanalysed == 1:
+                i.said.isanalysed = '已分析'
+            elif i.said.isanalysed == 0:
+                i.said.isanalysed = '未分析'
+            else:
+                i.said.isanalysed = '文件不全'
+            # 将检查数据写成文件
+            all_check = CheckInformation.objects.filter(source__idsurveyattribute=idsurveyattribute). \
+                order_by("-log_level")
+            with open(new_file_path + '分析报告.txt', 'w') as f:
+                for j in all_check:
+                    f.write(j.source.pjid + ' ' + j.source.pjname + '  数据类型：' + j.source.filetype + '  状态：'+ i.said.isanalysed+ '\n')
+                    f.write(i.filepath + '\n')
+                    for x in all_check:
+                        f.write(x.log_level + ' : ' + x.check_category  + '   ' + x.information+ '   ' + x.detail + '   ' + '\n')
+                    break
+        file_name = "分析报告.txt"  # 文件名
+        new_file_path = os.path.join(file_path,file_name)  # 下载文件的绝对路径
+    elif type=='2':
+        # 取出点位数据
+        file_points = Points2018.objects.filter(source__idsurveyattribute=idsurveyattribute)
+        with open(new_file_path + '点位数据.txt', 'w') as f:
+            for i in file_points:
+                f.write(i.wkbgeometry + ',' + i.source.pjid)
+                f.write('\n')
+        file_name = "点位数据.txt"  # 文件名
+        new_file_path = os.path.join(file_path, file_name)  # 下载文件的绝对路径
+    else:
+        # 取出外业文件
+        all_files = Surveyfile.objects.filter(said__idsurveyattribute=idsurveyattribute)
+        # 将外业文件写出来
+        for i in all_files:
+            path = os.path.split(i.filepath)
+            file_name =path[1]  # 文件名
+            new_file_path = os.path.join(file_path, '外业文件\\')  # 下载文件的绝对路径
+            if not os.path.exists(new_file_path):  # 判断文件夹是否存在
+                os.makedirs(new_file_path)
+            with open(new_file_path+file_name, 'wb') as f:
+                f.write(bytes(i.content))
+        name=os.path.join(file_path, '外业原始文件.zip')
+        zip_ya(new_file_path,name)
+        file_name='外业原始文件.zip'
+        new_file_path = os.path.join(file_path, '外业原始文件.zip')  # 下载文件的绝对路径
+
     if not os.path.isfile(new_file_path):  # 判断下载文件是否存在
-        print(new_file_path)
         return HttpResponse("Sorry but Not Found the File")
+
+
 
     def file_iterator(file_path, chunk_size=512):
         with open(file_path, mode='rb') as f:
@@ -162,5 +177,4 @@ def analysis_report_down(request,idsurveyattribute):
         response['Content-Disposition'] = 'attachment;filename="{}"'.format(file_name)
     except:
         return HttpResponse("Sorry but Not Found the File")
-
     return response
